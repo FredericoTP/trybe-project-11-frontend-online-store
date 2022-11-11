e7d3dcee0ae0dc708a3fdaea28ec2a036f4b93b0
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-11 13:33:30 -0300

    Requisito 4

--- frontend-online-store/src/pages/Home.jsx
+++ frontend-online-store/src/pages/Home.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  state = {
+    categories: [],
+  };
+
+  async componentDidMount() {
+    const categories = await getCategories();
+
+    this.setState({
+      categories,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <Header />
+        <div>
+          {
+            categories.map((category) => {
+              const { id, name } = category;
+              return (
+                <div key={ id }>
+                  <button
+                    data-testid="category"
+                    type="button"
+                  >
+                    { name }
+                  </button>
+                </div>
+              );
+            })
+          }
+        </div>
         <div>
           <p
             data-testid="home-initial-message"
